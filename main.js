e8d2c63f5b10963d8b06b76ba1b16bed987ebef5
sandeeepgithub <sandeeepgithub@gmail.com>
2024-04-18 14:47:53 +0530

    commit 2

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-console.log("I am dev 02 with my changes")
-console.log("hello world")
+console.log("I am dev 02 with my changes");
+console.log("hello world");
 
-console.log("commit one")
+console.log("commit one");
+
+console.log("making one more commit");
